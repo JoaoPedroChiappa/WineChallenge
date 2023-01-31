f63e229d45db0462a4db1d1335d4dc71a30b560f
JoaoPedroChiappa <joaopedrochiappa@edu.uniube.br>
2023-01-31 15:00:40 -0300

    HomePage part2

--- wine-challenge/pages/_app.js
+++ wine-challenge/pages/_app.js
@@ -1,8 +1,7 @@
-import type { AppProps } from "next/app";
 import "../styles/globals.css";
 import { Header } from "@/components/header/header";
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }) {
   return (
     <>
       <Header />
--- wine-challenge/src/components/home/home-page.js
+++ wine-challenge/src/components/home/home-page.js
@@ -1,4 +1,7 @@
-export const HomePage = () => (
+import Link from "next/link";
+import Image from "next/image";
+
+export const HomePage = ({ data }) => (
   <div className="home-page">
     <div className="side-menu">
       <h4> Refine sua busca </h4>
@@ -19,6 +22,22 @@ export const HomePage = () => (
       <label htmlFor="priceRange5"> Acima de R$500 </label>
     </div>
 
-    <div className="product-menu"></div>
+    <div className="product-menu">
+      {data?.map((ev) => (
+        <div className="single-product" key={ev.id}>
+          <Link href={`/events/${ev.id}`} passHref legacyBehavior>
+            <a className="card" href={`/events/${ev.id}`}>
+              <div className="image">
+                <Image width={250} height={200} alt={ev.title} src={ev.image} />
+              </div>
+              <div className="content">
+                <h2> {ev.title} </h2>
+                <p> R${ev.price} </p>
+              </div>
+            </a>
+          </Link>
+        </div>
+      ))}
+    </div>
   </div>
 );
